fb137c88574650d88515c4dec4faef72b3447d37
JaakkoMal <j.j.malmi@gmail.com>
2023-02-17 19:16:36 +0200

    Added and styled the initial view
    Added and styled the initial view when app opened.

--- App.tsx
+++ App.tsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import { SafeAreaView, StyleSheet, StatusBar } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native'
-import LoginView from './src/views/LoginView'
+import StartView from './src/views/StartView'
 import MainNavigator from './src/navigation/TabNavigator'
 
 export default function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false)
   
-  if (!isLoggedIn) return <LoginView />
+  if (!isLoggedIn) return (
+      <StartView />
+  )
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar 
