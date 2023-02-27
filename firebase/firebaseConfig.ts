bcf3f5e1e7dc62df93979947e1d388cb65f4d5bc
JaakkoMal <j.j.malmi@gmail.com>
2023-02-27 20:09:43 +0200

    Update firebase config
    Accidentally left API key in Firebase config. Removed, deleted project from Firebase and created new one..

--- firebase/firebaseConfig.ts
+++ firebase/firebaseConfig.ts
@@ -4,15 +4,9 @@ import { getReactNativePersistence, initializeAuth } from 'firebase/auth/react-n
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth"
 import { getFirestore, doc, getDoc, setDoc, updateDoc, arrayUnion, arrayRemove } from 'firebase/firestore'
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAYXlAuvtPcQW6NFEDPF5j53sHjItXbxR0",
-    authDomain: "appness-fit.firebaseapp.com",
-    projectId: "appness-fit",
-    storageBucket: "appness-fit.appspot.com",
-    messagingSenderId: "301835086613",
-    appId: "1:301835086613:web:f86b43b069525174e9d71c"
-};
+  // Your Firebase config here..  
+  };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
